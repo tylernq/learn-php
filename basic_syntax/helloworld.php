802c9edb3d49c60ee98d8a800b1fc399705ab70c
tylernq <tyler.github@gmail.com>
2018-07-19 00:35:04 +0800

    learn php types of date

--- basic_syntax/helloworld.php
+++ basic_syntax/helloworld.php
@@ -1,3 +1,38 @@
 <?php
     echo 'hello world';
 ?>
+<!-- // 另外支持的几种PHP标记 -->
+
+<?
+    echo "这是短标记";
+?>
+<script language="php">
+        echo 'script 标记';
+</script>
+
+
+
+
+<!-- 条件高级分离 -->
+<?php $expression = true ?>
+<?php if ($expression == true): ?>
+  This will show if the expression is true.
+<?php else: ?>
+  Otherwise this will show.
+<?php endif; ?>
+
+
+
+<!-- 去掉结束标记，减少意外输出 -->
+<?php
+echo "hello world";
+
+// more code.....
+
+echo "Last statement";
+
+//脚本至此结束，并无PHP结束标记，无结束标记可以避免结束标记之后的意外输出情况，输出空行或换行符。
+
+
+
+
